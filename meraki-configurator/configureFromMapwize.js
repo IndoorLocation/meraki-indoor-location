739be1c14555c7270679f25ed42bf2c0123dae33
Maxime Colmant <maxime.colmant@gmail.com>
2018-07-29 22:09:30 +0200

    refactor: update code to work with the latest version of the Meraki API (#8)

--- meraki-configurator/configureFromMapwize.js
+++ meraki-configurator/configureFromMapwize.js
@@ -69,7 +69,7 @@ async.series([
             }
             else {
                 var json = JSON.parse(data);
-                merakiFloorPlansByName = _.keyBy(json.floorplans, 'name');
+                merakiFloorPlansByName = _.keyBy(json, 'name');
                 next();
             }
         });
