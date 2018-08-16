1319fe7419b14389613aff77bee0ff77331fbcb4
Mathieu Gerard <mathieu@mapwize.io>
2018-08-16 17:44:40 +0200

    Fix merge issue

--- meraki-server/config/env/all.js
+++ meraki-server/config/env/all.js
@@ -30,7 +30,6 @@ module.exports = {
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
-        table: process.env.MYSQL_TABLE
         table: process.env.MYSQL_TABLE,
         poolConnectionLimit: process.env.MYSQL_POOL || 10
     }
