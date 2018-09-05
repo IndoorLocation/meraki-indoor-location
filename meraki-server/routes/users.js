63bd94c8b68c931a2ed673f18c2c6f114476ea58
Mathieu Gerard <mathieu@mapwize.io>
2018-09-05 17:19:34 +0200

    Fixing logs in users routes

--- meraki-server/routes/users.js
+++ meraki-server/routes/users.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var cache = require('../cache');
+var utils = require('../utils');
 
 exports.list = function (req, res) {
 
     cache.getKeys( function(err, keys) {
         if (err) {
-            util.log(err);
+            utils.log(err);
         }
 
         res.status(200).send(keys);
@@ -18,7 +19,7 @@ exports.user = function(req, res) {
 
     cache.getObject(req.params.userId, function(err, object){
         if (err) {
-            util.log(err);
+            utils.log(err);
         }
 
         res.status(200).send(object);
